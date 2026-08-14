11dcaa4218eeb5f09e95c4344d6a8a65233e5975
mfarhanafzal809-ctrl <mfarhanafzal809@gmail.com>
2026-08-14 21:49:26 +0500

    Update MainActivity.kt

--- app/src/main/java/com/ultratools/app/MainActivity.kt
+++ app/src/main/java/com/ultratools/app/MainActivity.kt
@@ -1,1 +1,27 @@
+package com.ultratools.app
 
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.ultratools.app.ui.shell.UltraToolsApp
+import com.ultratools.app.ui.theme.UltraToolsTheme
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        installSplashScreen()
+
+        super.onCreate(savedInstanceState)
+
+        enableEdgeToEdge()
+
+        setContent {
+            UltraToolsTheme {
+                UltraToolsApp()
+            }
+        }
+    }
+}
