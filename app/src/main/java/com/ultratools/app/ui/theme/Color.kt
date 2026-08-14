acedb6f9f57aef593d132ae92f4d7b592f2dba4a
mfarhanafzal809-ctrl <mfarhanafzal809@gmail.com>
2026-08-14 22:47:40 +0500

    Update Color.kt

--- app/src/main/java/com/ultratools/app/ui/theme/Color.kt
+++ app/src/main/java/com/ultratools/app/ui/theme/Color.kt
@@ -1,1 +1,21 @@
+package com.ultratools.app.ui.theme
 
+import androidx.compose.ui.graphics.Color
+
+val UltraBackgroundDark = Color(0xFF090B10)
+val UltraSurfaceDark = Color(0xFF12151D)
+val UltraSurfaceVariantDark = Color(0xFF1A1F29)
+
+val UltraPrimary = Color(0xFF6E8CFF)
+val UltraPrimaryBright = Color(0xFF9CB1FF)
+
+val UltraSecondary = Color(0xFF7ED9C2)
+
+val UltraTextPrimary = Color(0xFFF4F6FB)
+val UltraTextSecondary = Color(0xFFAAB2C2)
+
+val UltraBackgroundLight = Color(0xFFF7F8FC)
+val UltraSurfaceLight = Color(0xFFFFFFFF)
+val UltraSurfaceVariantLight = Color(0xFFECEFF5)
+
+val UltraError = Color(0xFFFF6B6B)
