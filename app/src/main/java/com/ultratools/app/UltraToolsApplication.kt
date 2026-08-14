25296b3ba4231518fda3450dd9847928f50942c9
mfarhanafzal809-ctrl <mfarhanafzal809@gmail.com>
2026-08-14 21:45:43 +0500

    Update UltraToolsApplication.kt

--- app/src/main/java/com/ultratools/app/UltraToolsApplication.kt
+++ app/src/main/java/com/ultratools/app/UltraToolsApplication.kt
@@ -1,1 +1,15 @@
+package com.ultratools.app
 
+import android.app.Application
+import com.ultratools.app.core.config.AppConfiguration
+
+class UltraToolsApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        AppConfiguration.initialize(
+            applicationContext = applicationContext
+        )
+    }
+}
